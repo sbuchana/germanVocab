0414d01f9aa654a2ad4c3cffa9ef5eee5c72387b
Sandy Buchanan <s.buchanan@alumni.ubc.ca>
2014-03-14 12:07:20 -0700

    Test2

---
+++ Vocab/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vocab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            vocabText.Text = "";
+        }
+
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            vocabText.Text = textBoxInput.Text;
+            textBoxInput.Text = "";
+        }
+
+        private void uUmlautButton_Click(object sender, EventArgs e)
+        {
+            textBoxInput.AppendText("ü");
+            textBoxInput.Focus();
+        }
+
+        private void oUmlautButton_Click(object sender, EventArgs e)
+        {
+            textBoxInput.AppendText("ö");
+            textBoxInput.Focus();
+        }
+
+        private void aUmlautButton_Click(object sender, EventArgs e)
+        {
+            textBoxInput.AppendText("ä");
+            textBoxInput.Focus();
+        }
+
+        private void eszettButton_Click(object sender, EventArgs e)
+        {
+            textBoxInput.AppendText("ß");
+            textBoxInput.Focus();
+        }
+
+        private void uCapUmlautButton_Click(object sender, EventArgs e)
+        {
+            textBoxInput.AppendText("Ü");
+            textBoxInput.Focus();
+        }
+
+        private void oCapUmlautButton_Click(object sender, EventArgs e)
+        {
+            textBoxInput.AppendText("Ö");
+            textBoxInput.Focus();
+        }
+
+        private void aCapUmlautButton_Click(object sender, EventArgs e)
+        {
+            textBoxInput.AppendText("Ä");
+            textBoxInput.Focus();
+        }
+
+    }
+}
